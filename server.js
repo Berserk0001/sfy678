ed4aad576dd027de211b6cde1584525661b245e8
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 10:00:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 #!/usr/bin/env node
 'use strict';
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')();
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 
 const PORT = process.env.PORT || 8080;
 
-fastify.get('/', async (req, reply) => {
-  await params(req, reply);
-  return proxy(req, reply);
-});
+fastify.register(require('@fastify/formbody'))
 
-fastify.get('/favicon.ico', (req, reply) => {
-  reply.status(204).send();
-});
+fastify.get('/', { preHandler: params }, proxy);
+fastify.get('/favicon.ico', (req, res) => res.status(204).send());
 
-fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Server listening at ${address}`);
-});
+fastify.listen({ port: PORT }, (err, address) => {
+  if (err) throw err
+  console.log(`Server listening at ${address}`)
+})
