802f5178c519e0f18a4d47fb19be89acb4f4419e
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 13:09:37 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,9 +1,9 @@
-function bypass(req, res, buffer) {
-    res.setHeader('x-proxy-bypass', 1);
-    res.setHeader('content-length', buffer.length);
-    res.status(200);
-    res.write(buffer);
-    res.end();
+function bypass(request, reply, buffer) {
+    reply
+        .header('x-proxy-bypass', 1)
+        .header('content-length', buffer.length)
+        .status(200)
+        .send(buffer);
 }
 
 module.exports = bypass;
