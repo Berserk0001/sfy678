8dda1d3cc787dc73d49aa80c8e30f5ca18dca38a
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 08:52:19 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,35 +1,30 @@
 "use strict";
-/*
- * proxy.js
- * The bandwidth hero proxy handler.
- * proxy(httpRequest, httpResponse);
- */
-const { request } = require("undici");
-const pick = require("lodash/pick");
+
+const undici = require("undici");
+const pick = require("lodash").pick;
 const shouldCompress = require("./shouldCompress");
 const redirect = require("./redirect");
 const compress = require("./compress");
 const copyHeaders = require("./copyHeaders");
 
 async function proxy(req, reply) {
-  if (
-    req.headers["via"] == "1.1 bandwidth-hero" &&
-    ["127.0.0.1", "::1"].includes(req.headers["x-forwarded-for"] || req.ip)
-  ) {
+  if (req.headers["via"] == "1.1 bandwidth-hero" && 
+      ["127.0.0.1", "::1"].includes(req.headers["x-forwarded-for"] || req.ip)) {
     return redirect(req, reply);
   }
 
   try {
-    const origin = await request(req.params.url, {
+    let origin = await undici.request(req.body.url, {
       headers: {
         ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
         "user-agent": "Bandwidth-Hero Compressor",
         "x-forwarded-for": req.headers["x-forwarded-for"] || req.ip,
         via: "1.1 bandwidth-hero",
       },
-      maxRedirections: 4,
+      maxRedirections: 4
     });
-    _onRequestResponse(origin, req, reply);
+
+    await _onRequestResponse(origin, req, reply);
   } catch (err) {
     _onRequestError(req, reply, err);
   }
@@ -37,48 +32,42 @@ async function proxy(req, reply) {
 
 function _onRequestError(req, reply, err) {
   if (err.code === "ERR_INVALID_URL") {
-    return reply.code(400).send("Invalid URL");
+    return reply.code(400).send({ error: "Invalid URL" });
   }
-
-  // When there's a real error, redirect then destroy the stream immediately.
   redirect(req, reply);
   console.error(err);
 }
 
-function _onRequestResponse(origin, req, reply) {
+async function _onRequestResponse(origin, req, reply) {
   if (origin.statusCode >= 400) {
     return redirect(req, reply);
   }
 
-  // handle redirects
   if (origin.statusCode >= 300 && origin.headers.location) {
     return redirect(req, reply);
   }
 
   copyHeaders(origin, reply);
-  reply.header("content-encoding", "identity");
+  reply.header('content-encoding', 'identity');
   reply.header("Access-Control-Allow-Origin", "*");
   reply.header("Cross-Origin-Resource-Policy", "cross-origin");
   reply.header("Cross-Origin-Embedder-Policy", "unsafe-none");
-  req.params.originType = origin.headers["content-type"] || "";
-  req.params.originSize = origin.headers["content-length"] || "0";
 
-  origin.body.on('error', () => req.socket.destroy());
+  req.body.originType = origin.headers["content-type"] || "";
+  req.body.originSize = origin.headers["content-length"] || "0";
+
+  origin.body.on('error', () => req.destroy());
 
   if (shouldCompress(req)) {
-    // sharp supports stream, so pipe it.
-    return compress(req, reply, origin);
+    return compress(req, reply, origin.body);
   } else {
-    // For cases where we bypass compression.
-    reply.header("x-proxy-bypass", 1);
-
+    reply.header("x-proxy-bypass", "1");
     for (const headerName of ["accept-ranges", "content-type", "content-length", "content-range"]) {
-      if (headerName in origin.headers) {
+      if (origin.headers[headerName]) {
         reply.header(headerName, origin.headers[headerName]);
       }
     }
-
-    return origin.body.pipe(reply.raw);
+    reply.send(origin.body); // Fastify handles streams differently, directly send the stream.
   }
 }
 
