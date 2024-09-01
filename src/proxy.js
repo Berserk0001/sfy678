e906abbcb8e280208d5f18210191be23efaf5504
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 14:16:16 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -6,7 +6,22 @@ const compress = require('./compress');
 const bypass = require('./bypass');
 const copyHeaders = require('./copyHeaders');
 
+const DEFAULT_QUALITY = 1;
+
 async function proxy(request, reply) {
+    let url = request.query.url;
+    if (Array.isArray(url)) url = url.join('&url=');
+    if (!url) {
+        reply.send('bandwidth-hero-proxy');
+        return;
+    }
+
+    url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+    request.params.url = url;
+    request.params.webp = !request.query.jpeg;
+    request.params.grayscale = request.query.bw != 0;
+    request.params.quality = parseInt(request.query.l, 10) || DEFAULT_QUALITY;
+
     try {
         const response = await fetch(request.params.url, {
             headers: {
