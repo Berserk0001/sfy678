5f7201b345567f24ec1f0898a5e303dd8f4b4f25
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 11:28:10 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,14 +1,13 @@
-"use strict";
 function redirect(req, res) {
-  if (res.sent) return;
+    if (res.headersSent) return;
 
-  res.header('content-length', 0);
-  res.removeHeader('cache-control');
-  res.removeHeader('expires');
-  res.removeHeader('date');
-  res.removeHeader('etag');
-  res.header('location', encodeURI(req.params.url));
-  res.status(302).send();
+    res.setHeader('content-length', 0);
+    res.removeHeader('cache-control');
+    res.removeHeader('expires');
+    res.removeHeader('date');
+    res.removeHeader('etag');
+    res.setHeader('location', encodeURI(req.params.url));
+    res.status(302).end()
 }
 
-module.exports = redirect
+module.exports = redirect;
