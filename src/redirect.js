9530ae240facd15d1d9c1338edbeda658f7ba806
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 09:41:15 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,12 +1,16 @@
 "use strict";
-
 function redirect(req, reply) {
-  // Check if the response has not already been sent
-  if (!reply.sent) {
-    reply.header('location', encodeURI(req.body.url));
-    reply.removeHeader('content-length'); // Reset content length as per Fastify conventions
-    reply.code(302).send();
-  }
+  if (reply.sent) return;
+
+  reply
+    .header('content-length', 0)
+    .removeHeader('cache-control')
+    .removeHeader('expires')
+    .removeHeader('date')
+    .removeHeader('etag')
+    .header('location', encodeURI(req.params.url))
+    .status(302)
+    .send();
 }
 
 module.exports = redirect;
