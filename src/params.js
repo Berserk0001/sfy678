b1abdf883bc762271abe3c48f69bbd777c7536fd
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 13:11:15 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,17 +1,18 @@
 const DEFAULT_QUALITY = 1;
 
-function params(req, res, next) {
-    let url = req.query.url;
+async function params(request, reply) {
+    let url = request.query.url;
     if (Array.isArray(url)) url = url.join('&url=');
-    if (!url) return res.end('bandwidth-hero-proxy');
+    if (!url) {
+        reply.send('bandwidth-hero-proxy');
+        return;
+    }
 
     url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-    req.params.url = url;
-    req.params.webp = !req.query.jpeg;
-    req.params.grayscale = req.query.bw != 0;
-    req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
-
-    next()
+    request.params.url = url;
+    request.params.webp = !request.query.jpeg;
+    request.params.grayscale = request.query.bw != 0;
+    request.params.quality = parseInt(request.query.l, 10) || DEFAULT_QUALITY;
 }
 
 module.exports = params;
