b001952c50ce137cee4b45977ee26b5558b5f8de
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 11:26:10 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,43 +1,25 @@
-"use strict";
-/*
- * compress.js
- * A module that compress a image.
- * compress(httpRequest, httpResponse, ReadableStream);
- */
-const sharp = require('sharp')
-const redirect = require('./redirect')
+const sharp = require('sharp');
+const redirect = require('./redirect');
 
-const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
-
-async function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
-
-  try {
-      const output = await input.body.pipe(sharpStream()
-          .grayscale(req.params.grayscale)
-          .toFormat(format, {
-              quality: req.params.quality,
-              progressive: true,
-              optimizeScans: true
-          })
-          .toBuffer())
-
-      const info = { size: output.length }; // Get size from buffer length
-
-      _sendResponse(null, output, info, format, req, res);
-  } catch (err) {
-      _sendResponse(err, null, null, format, req, res);
-  }
-}
-
-function _sendResponse(err, output, info, format, req, res) {
-  if (err || !info) return redirect(req, res);
-
-  res.header('content-type', 'image/' + format);
-  res.header('content-length', info.size);
-  res.header('x-original-size', req.params.originSize);
-  res.header('x-bytes-saved', req.params.originSize - info.size);
-  res.status(200).send(output);
+function compress(req, res, input) {
+    const format = req.params.webp ? 'webp' : 'jpeg';
+    sharp(input)
+        .grayscale(req.params.grayscale)
+        .toFormat(format, {
+            quality: req.params.quality,
+            progressive: true,
+            optimizeScans: true
+        })
+        .toBuffer((err, output, info) => {
+            if (err || !info || res.headersSent) return redirect(req, res);
+            res.setHeader('content-type', `image/${format}`);
+            res.setHeader('content-length', info.size);
+            res.setHeader('x-original-size', req.params.originSize);
+            res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+            res.status(200);
+            res.write(output);
+            res.end()
+        })
 }
 
 module.exports = compress;
