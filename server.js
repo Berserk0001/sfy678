75733a8273518c5555cc5412f6ab0f992dae7e53
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 09:50:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,26 +7,15 @@ const proxy = require('./src/proxy');
 const PORT = process.env.PORT || 8080;
 
 fastify.get('/', async (req, reply) => {
-  try {
-    await params(req, reply);
-    if (!reply.sent) {
-      return proxy(req, reply);
-    }
-  } catch (err) {
-    fastify.log.error(err);
-    if (!reply.sent) {
-      reply.status(500).send('Internal Server Error');
-    }
-  }
+  await params(req, reply);
+  return proxy(req, reply);
 });
 
 fastify.get('/favicon.ico', (req, reply) => {
-  if (!reply.sent) {
-    reply.status(204).send();
-  }
+  reply.status(204).send();
 });
 
-fastify.listen({ host: '0.0.0.0' , port: PORT }, (err, address) => {
+fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
