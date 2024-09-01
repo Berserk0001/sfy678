414e8dd4c73bf7b26014487642450672cd895c0b
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-01 08:53:58 +0530

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,10 +1,11 @@
 "use strict";
+
 function copyHeaders(source, target) {
   for (const [key, value] of Object.entries(source.headers)) {
     try {
       target.header(key, value);
     } catch (e) {
-      console.log(e.message);
+      console.log(`Failed to set header ${key}: ${e.message}`);
     }
   }
 }
